640337c6021b527f86d10f026abfd4a1cb0140e7
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-12 13:33:04 +0200

    test files

---
+++ tests/test_models.py/test_amenity.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Test for amenities.
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    Tests amenities
+    """
+
+    def test_name(self):
+        """
+        Tests input names
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
