dd42139d737dc3a12ff22f7d1ccf1d3818b9b995
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-12 14:13:25 +0200

    fixed errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,57 +1,32 @@
-#!/usr/bin/python3
-"""
-Module file_storage serializes and
-deserializes JSON types
-"""
-
 import json
 from models.base_model import BaseModel
 from models.user import User
 
-
 class FileStorage:
-    """
-    Custom class for file storage
-    """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns dict representation of all obj
-        """
         return self.__objects
 
-    def new(self, object):
-        """sets in __objects the object with the key
-        <object class name>.id
-
-        Args:
-            object(obj): object to write
-
-        """
-        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
+    def new(self, obj):
+        key = obj.__class__.__name__ + '.' + str(obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file
-        (path: __file_path)
-        """
-        with open(self.__file_path, 'w+') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()
-                       }, f)
+        with open(self.__file_path, 'w') as f:
+            json.dump({key: obj.to_dict() for key, obj in self.__objects.items()}, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects, if the JSON
-        file exists, otherwise nothing happens)
-        """
         try:
             with open(self.__file_path, 'r') as f:
-                dict = json.loads(f.read())
-                for value in dict.values():
-                    cls = value["__class__"]
-                    self.new(eval(cls)(**value))
-        except Exception:
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name = value["__class__"]
+                    if class_name == 'BaseModel':
+                        self.__objects[key] = BaseModel(**value)
+                    elif class_name == 'User':
+                        self.__objects[key] = User(**value)
+                    # Add more elif conditions for other classes
+        except FileNotFoundError:
             pass
